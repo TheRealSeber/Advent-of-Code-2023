7d05f838be799c3480da3117db9edabd41f8ffd6
TheRealSeber <rydzseba@gmail.com>
2023-12-02 03:16:08 +0100

    day-01 solution

---
+++ day-01/src/part1.rs
@@ -0,0 +1,37 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(
+    input: &str,
+) -> miette::Result<String, AocError> {
+    Ok(input.lines().fold(0_i32, |acc, x| {
+        let res = x.chars().fold((0_u8, 0_u8), |(x, y), ch| {
+            match ch.to_digit(10) {
+                Some(v) => {
+                    if x != 0 {
+                        (x, v as u8)
+                    } else {
+                        (v as u8, v as u8)
+                    }
+                }
+                None => (x, y)
+            }
+        });
+        acc + (res.0 * 10 + res.1) as i32
+    }).to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!("142", process(input)?);
+        Ok(())
+    }
+}
---
+++ day-01/src/part2.rs
@@ -0,0 +1,49 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(
+    input: &str,
+) -> miette::Result<String, AocError> {
+    let num_str = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    Ok(input.lines().fold(0_i32, |acc, x| {
+        let (mut l_dg,  mut r_dg) = (0_u8, 0_u8);
+        for i in 0..x.len() {
+            for (idx, s )in num_str.iter().enumerate() {
+                if x[i..].starts_with(s) {
+                    if l_dg != 0 {
+                        r_dg = (idx + 1) as u8;
+                    } else {
+                        (l_dg, r_dg) = ((idx + 1) as u8, (idx + 1) as u8);
+                    }
+                    break;
+                }
+            }
+            if let Ok(v) = x[i..i+1].parse::<u8>() {
+                if l_dg != 0 {
+                    r_dg = v as u8;
+                } else {
+                    (l_dg, r_dg) = (v as u8, v as u8);
+                }
+            }
+        }
+        acc + (l_dg * 10 + r_dg) as i32
+    }).to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
+        Ok(())
+    }
+}
