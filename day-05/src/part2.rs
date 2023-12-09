e2b87217ec29d32a684f0ef1902776c14e9a6e80
TheRealSeber <rydzseba@gmail.com>
2023-12-09 23:59:43 +0100

    day-05 part2 - not ending

--- day-05/src/part2.rs
+++ day-05/src/part2.rs
@@ -1,8 +1,83 @@
 use crate::custom_error::AocError;
+use nom::{
+    bytes::complete::take_until,
+    character::complete::{self, line_ending, space1},
+    multi::{many1, separated_list1},
+    sequence::{tuple, separated_pair},
+    IResult, Parser,
+};
+use nom_supreme::{tag::complete::tag, ParserExt};
+use std::ops::Range;
+
+#[derive(Debug)]
+struct SeedMap {
+    mappings: Vec<(Range<u64>, Range<u64>)>,
+}
+
+impl SeedMap {
+    fn get_map(&self, source: u64) -> u64 {
+        let valid_mapping = self
+            .mappings
+            .iter()
+            .find(|(source_map, _)| source_map.contains(&source));
+        match valid_mapping {
+            Some((source_map, destination_map)) => {
+                destination_map.start + (source - source_map.start)
+            }
+            None => source,
+        }
+    }
+}
+
+fn create_map(input: &str) -> IResult<&str, (Range<u64>, Range<u64>)> {
+    let (input, (destination, source, range)) = tuple((
+        complete::u64,
+        complete::u64.preceded_by(tag(" ")),
+        complete::u64.preceded_by(tag(" ")),
+    ))(input)?;
+
+    Ok((
+        input,
+        (
+            (source..source + range),
+            (destination..source + destination),
+        ),
+    ))
+}
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+fn seed_map(input: &str) -> IResult<&str, SeedMap> {
+    take_until("map:")
+        .precedes(tag("map:"))
+        .precedes(many1(line_ending.precedes(create_map)).map(|mappings| SeedMap { mappings }))
+        .parse(input)
+}
+
+#[tracing::instrument]
+fn parse_seeds(input: &str) -> IResult<&str, (Vec<SeedMap>, Vec<Range<u64>>)> {
+    let (input, seeds) = tag("seeds: ")
+        .precedes(separated_list1(space1, separated_pair(complete::u64, tag(" "), complete::u64).map(|(x , y)| (x..x+y))))
+        .parse(input)?;
+    let (input, maps) = many1(seed_map)(input)?;
+    Ok((input, (maps, seeds)))
+}
+#[tracing::instrument(skip(input), fields(input_first_line = input.split('\n').next().unwrap()))]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let (_, (seedmaps, seeds)) = parse_seeds(input).expect("Should be valid");
+    let min_loc = seeds
+        .into_iter()
+        .fold(u64::MAX, |min_value, chunk| {
+                chunk
+                .map(|seed| {
+                    seedmaps
+                        .iter()
+                        .fold(seed, |seed, seed_map| seed_map.get_map(seed))
+                })
+                .min()
+                .expect("Should exist")
+                .min(min_value)
+        });
+    Ok(min_loc.to_string())
 }
 
 #[cfg(test)]
@@ -11,9 +86,40 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+        assert_eq!("46", process(input)?);
         Ok(())
     }
 }
