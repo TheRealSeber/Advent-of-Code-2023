d594f964aa487b7b1d55f1b37ddf3900aeb99fdb
TheRealSeber <rydzseba@gmail.com>
2023-12-09 23:59:02 +0100

    day-09

---
+++ day-09/src/part2.rs
@@ -0,0 +1,38 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let result = input
+        .lines()
+        .map(|k| {
+            k.split_whitespace()
+                .map(|v| v.parse::<i32>().expect("Should be valid"))
+                .collect::<Vec<i32>>()
+        })
+        .fold(0, |sum, x| {
+            sum + x.first().expect("Must exist") - vectorize(&x)
+        });
+    Ok(result.to_string())
+}
+
+fn vectorize(slice: &[i32]) -> i32 {
+    let new_vec = slice.windows(2).map(|k| k[1] - k[0]).collect::<Vec<i32>>();
+    match new_vec.iter().all(|v| *v == 0) {
+        true => 0,
+        false => new_vec.first().expect("Should exist") - vectorize(&new_vec)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!("2", process(input)?);
+        Ok(())
+    }
+}
