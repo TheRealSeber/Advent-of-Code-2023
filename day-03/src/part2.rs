dd115e643bc816f3290cf18b351ce87343a4da58
TheRealSeber <rydzseba@gmail.com>
2023-12-24 15:44:56 +0100

    day-03 part2

--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,92 +1,110 @@
+use std::collections::HashSet;
+
 use nom::AsChar;
 
 use crate::custom_error::AocError;
 
-fn construct_num(v: &[u8], idx: i32) -> u32 {
-    let mut res: (i32, i32) = (idx, idx);
-    if idx != 0 {
-        while let Some(k) = v.get((res.0 - 1) as usize) {
-            match k.is_dec_digit() {
-                true => {
-                    res.0 -= 1;
-                    if res.0 == 0 {
-                        break;
-                    }
-                }
-                false => break,
-            }
-        }
-    }
-    while let Some(k) = v.get((res.1 + 1) as usize) {
-        match k.is_dec_digit() {
-            true => {
-                res.1 += 1;
-            }
-            false => break,
-        }
-    }
-    unsafe {
-        String::from_utf8_unchecked(v[res.0 as usize..(res.1 + 1) as usize].to_vec())
-            .parse::<u32>()
-            .expect("Should be valid")
-    }
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Entry {
+    Default,
+    Number(u32),
+    Star,
 }
 
-fn crawler(v: &[&[u8]], idx: (i32, i32)) -> u32 {
-    let neighbour_up_down = [-1, 0, 1];
-    let neighbout_line = [-1, 1];
-    let mut numbers = vec![];
-    for x in neighbour_up_down {
-        if let Some(row) = v.get((idx.1 + 1) as usize) {
-            if let Some(num) = row.get((idx.0 + x).min(0) as usize) {
-                if num.is_dec_digit() {
-                    numbers.push(construct_num(row, (idx.0 + x).min(0)));
-                    break;
-                }
+fn neighbours_product((y, x): &(usize, usize), map: &Vec<Vec<Entry>>) -> u32 {
+    let mut neighbours = HashSet::new();
+    if *y > 0 {
+        if let Entry::Number(num) = map[*y - 1][*x] {
+            neighbours.insert(num);
+        }
+        if *x > 0 {
+            if let Entry::Number(num) = map[*y - 1][*x - 1] {
+                neighbours.insert(num);
+            }
+        }
+        if *x + 1 < map[0].len() {
+            if let Entry::Number(num) = map[*y - 1][*x + 1] {
+                neighbours.insert(num);
             }
         }
     }
-    for x in neighbout_line {
-        if let Some(row) = v.get(idx.1 as usize) {
-            if idx.0 + x == -1 {
-                continue;
-            }
-            if let Some(num) = row.get((idx.0 + x) as usize) {
-                if num.is_dec_digit() {
-                    numbers.push(construct_num(row, (idx.0 + x).min(0)));
-                    break;
-                }
+    if *y + 1 < map.len() {
+        if let Entry::Number(num) = map[*y + 1][*x] {
+            neighbours.insert(num);
+        }
+        if *x > 0 {
+            if let Entry::Number(num) = map[*y + 1][*x - 1] {
+                neighbours.insert(num);
             }
         }
-    }
-    for x in neighbour_up_down {
-        if idx.1 - 1 == -1 {
-            continue;
-        }
-        if let Some(row) = v.get((idx.1 - 1) as usize) {
-            if let Some(num) = row.get((idx.0 + x).min(0) as usize) {
-                if num.is_dec_digit() {
-                    numbers.push(construct_num(row, (idx.0 + x).min(0)));
-                    break;
-                }
+        if *x + 1 < map[0].len() {
+            if let Entry::Number(num) = map[*y + 1][*x + 1] {
+                neighbours.insert(num);
             }
         }
     }
-    dbg!(&numbers);
-    match numbers.len().cmp(&2) {
-        std::cmp::Ordering::Less => 0,
-        _ => numbers.into_iter().product::<u32>(),
+    if *x > 0 {
+        if let Entry::Number(num) = map[*y][*x - 1] {
+            neighbours.insert(num);
+        }
+    }
+    if *x + 1 < map[0].len() {
+        if let Entry::Number(num) = map[*y][*x + 1] {
+            neighbours.insert(num);
+        }
+    }
+    if neighbours.len() == 2 {
+        neighbours.into_iter().fold(1, |acc, x| acc * x)
+    } else {
+        0
     }
 }
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let to_vec = input.lines().map(|a| a.as_bytes()).collect::<Vec<&[u8]>>();
-    let mut res = 0;
-    for i in 0..to_vec.len() {
-        for j in 0..to_vec.len() {
-            if to_vec[i][j] == b'*' {
-                res += crawler(&to_vec, (j as i32, i as i32))
+    let lines = input.lines().collect::<Vec<&str>>();
+    let mut map = vec![vec![Entry::Default; lines[0].len()]; lines.len()];
+    for (y, line) in lines.into_iter().enumerate() {
+        let mut num_starting_point = 0;
+        let mut in_num = false;
+        for (x, ch) in line.chars().enumerate() {
+            if ch == '*' {
+                map[y][x] = Entry::Star;
+            }
+            if ch.is_dec_digit() {
+                if !in_num {
+                    num_starting_point = x;
+                    in_num = true;
+                }
+                if x + 1 == line.len() && in_num {
+                    for i in num_starting_point..x + 1 {
+                        map[y][i] = Entry::Number(
+                            line[num_starting_point..x + 1]
+                                .parse::<u32>()
+                                .expect("Should be valid"),
+                        );
+                    }
+                }
+            }
+            if !ch.is_dec_digit() {
+                if in_num {
+                    for i in num_starting_point..x {
+                        map[y][i] = Entry::Number(
+                            line[num_starting_point..x]
+                                .parse::<u32>()
+                                .expect("Should be valid"),
+                        );
+                    }
+                }
+                in_num = false;
+            }
+        }
+    }
+    let mut res = 0_u32;
+    for (y, line) in map.iter().enumerate() {
+        for (x, entry) in line.iter().enumerate() {
+            if *entry == Entry::Star {
+                res += neighbours_product(&(y, x), &map);
             }
         }
     }
@@ -107,8 +125,8 @@ mod tests {
 .....+.58.
 ..592.....
 ......755.
-...$.*....
-.664.598..";
+...$.*.*..
+.664...598";
         assert_eq!("467835", process(input)?);
         Ok(())
     }
